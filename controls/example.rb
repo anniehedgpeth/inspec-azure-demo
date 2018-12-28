cc95fea960a74d00eaf51af4cc1847ef1e24125a
anniehedgpeth <annie.hedgpeth@gmail.com>
2018-12-28 15:34:23 -0700

    added controls

--- controls/example.rb
+++ controls/example.rb
@@ -3,20 +3,29 @@
 
 title 'Sample InSpec Azure Controls'
 
+resource_group = 'my-resources'
+
 control 'azurerm_virtual_machine' do
-  describe azurerm_virtual_machine(resource_group: 'my-resources', name: 'my-vm') do
+  describe azurerm_virtual_machine(resource_group: resource_group, name: 'my-vm') do
     it                                { should exist }
     its('type')                       { should eq 'Microsoft.Compute/virtualMachines' }
   end
 end
 
 control 'azure_network_security_group' do
-  describe azure_network_security_group(resource_group: 'my-resources', name: 'nsg') do
+  describe azure_network_security_group(resource_group: resource_group, name: 'nsg') do
     it                            { should exist }
     its('type')                   { should eq 'Microsoft.Network/networkSecurityGroups' }
     its('security_rules')         { should_not be_empty }
     its('default_security_rules') { should_not be_empty }
     it                            { should_not allow_rdp_from_internet }
     it                            { should_not allow_ssh_from_internet }
+  end
+end
+
+control 'azure_network_security_group' do
+  describe azurerm_virtual_network(resource_group: resource_group, name: 'my-network') do
+    it               { should exist }
+    its('location')  { should eq 'Central US' }
   end
 end
