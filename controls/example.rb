b2cc0034a92cf20a6dde0b0987ba9bc4a3dd86e9
Annie Hedgpeth <annie.hedgpeth@relativity.com>
2020-01-13 19:25:24 -0700

    changed controls for inspec-azure-terraform-demo

--- controls/example.rb
+++ controls/example.rb
@@ -1,19 +1,15 @@
 # encoding: utf-8
 # copyright: 2018, Annie Hedgpeth
 
-title 'Sample InSpec Azure Controls'
+control 'resources' do
+  resource_group = 'test-kitchen'
 
-resource_group = 'demo-resources'
-
-control 'azurerm_virtual_machine' do
-  describe azurerm_virtual_machine(resource_group: resource_group, name: 'my-vm') do
+  describe azurerm_virtual_machine(resource_group: resource_group, name: 'my_vm') do
     it                                { should exist }
     its('type')                       { should eq 'Microsoft.Compute/virtualMachines' }
   end
-end
 
-control 'azure_network_security_group' do
-  describe azure_network_security_group(resource_group: resource_group, name: 'nsg') do
+  describe azurerm_network_security_group(resource_group: resource_group, name: 'nsg') do
     it                            { should exist }
     its('type')                   { should eq 'Microsoft.Network/networkSecurityGroups' }
     its('security_rules')         { should_not be_empty }
@@ -21,10 +17,8 @@ control 'azure_network_security_group' do
     it                            { should_not allow_rdp_from_internet }
     it                            { should_not allow_ssh_from_internet }
   end
-end
 
-control 'azure_virtual_network' do
-  describe azurerm_virtual_network(resource_group: resource_group, name: 'my-network') do
+  describe azurerm_virtual_network(resource_group: resource_group, name: 'my_network') do
     it               { should exist }
     its('location')  { should eq 'centralus' }
   end
